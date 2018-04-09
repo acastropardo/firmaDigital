1002650d419d43c951fd2f9337e49a8f4c3a8a6b
Abraham Castro <abraham.castro@gmail.com>
2018-04-09 10:56:42 -0300

    cambios pulentos + user en front

--- src/UI/routes/user.js
+++ src/UI/routes/user.js
@@ -1,31 +1,41 @@
 var express = require('express');
+var app = express();
+
 var router = express.Router();
-//var session = require('express-session');
-//var app = express();
+var session = require('express-session');
+
 
 // Express Session
-/*app.use(session({
-    secret: 'secret',
-    saveUninitialized: true,
-    resave: true
-}));*/
+ app.use(session({
+     secret: 'secret',
+     saveUninitialized: true,
+     resave: true
+ }));
 
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
   console.log('Time: ', Date.now());
+
   next();
 });
 // define the user page route
 
-router.get('/', function(req, res) {
-  res.send(req.session);
-  res.send('Birds home page');
-});
+ router.get('/*', function(req, res) {
+   //res.send(req.session);
+   //console.log(req.params.name);
+   //res.send('Birds home page');
+   console.log(req.params[0]);
 
-router.get('/user', function(req, res) {
-  res.send(req.session);
-  res.send('Birds home page');
-});
+   console.log(JSON.parse(req.params[0]).name);
+
+   res.send(req.params);
+
+ });
+
+// router.get('/test', function(req, res) {
+//   //res.send(req.session);
+//   res.send('Birds test page');
+// });
 
 
 module.exports = router;
